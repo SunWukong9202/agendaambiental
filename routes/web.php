db063412fb3d2d7bd9fbcfd17a4831e9bacfcdfa
santiago <azahel_santiago@hotmail.com>
2024-03-04 02:16:13 -0600

    Agregue la vista de historial de materiales, junto con el modal del formulario para hacer nuevas solicitudes por materiales

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,34 @@ use Faker\Factory as Faker;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+$materiales = [
+    "Acero",
+    "Aluminio",
+    "Cobre",
+    "Vidrio",
+    "Plástico",
+    "Madera",
+    "Papel",
+    "Hierro",
+    "Bronce",
+    "Latón",
+    "Fibra de carbono",
+    "Cerámica",
+    "Tela",
+    "Nylon",
+    "Goma",
+    "Piedra",
+    "Teflón",
+    "Polietileno",
+    "Polipropileno",
+];
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function (){
+    [$users, $roles] = fakeUsers();
+    return view('welcome', compact('users'), compact('roles'));
 });
 
-
-Route::get('/perms', function (): Mixed {
+function fakeUsers() {
     $faker = Faker::create();
     $users = [];
     $roles = ['Admin', 'Becaro', 'Cliente'];
@@ -31,8 +52,41 @@ Route::get('/perms', function (): Mixed {
         ];
         $users[] = $user;
     }
+    return [$users, $roles];
+}
+
+function fakeRequests($materiales) {
+    $requests = [];
+    $faker = Faker::create();
+    $status = ['Aceptada', 'En curso', 'Rechazada'];
+    
+    for ($i = 0; $i < 10; $i++) {
+        $state = $status[rand(0, count($status) - 1)];
+        $req = [];
+        $req['material'] = $materiales[rand(0, count($materiales) - 1)];
+        $req['cantidad'] = round(rand(30, 100 * 5) / 100, 2);
+        $begin = $faker->dateTimeBetween('-1 month', '+1 month')->format('Y-m-d');
+        $req['estado'] = $state;
+        $req['solicitado'] = $begin;
+        $req['aprobado'] = $state === 'En curso' ? 'En espera' : date_modify(new DateTime($begin), rand(1, 14) . ' days')->format('Y-m-d');
+        
+        $requests[] = $req;
+    }
+    
+    return $requests;
+}
+
+
+ 
+Route::get('/perms', function () : Mixed {
+   [$users, $roles] = fakeUsers();
     //compact('users') => 'users' => $users
     return view('permissions', compact('users'), compact('roles'));
 })->name('permissions');
+
+Route::get('/solicitudes', function () use ($materiales) {
+    $requests = fakeRequests($materiales);
+    return view('requestMaterials', compact('requests', 'materiales'));
+});
 
 Route::view('/registro_evento', 'eventRegistration');
