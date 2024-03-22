f621905c67aeb1721d3cfd9316819a5ed23eda2b
santiago <azahel_santiago@hotmail.com>
2024-03-21 18:30:33 -0600

    Probando cambio a livewire

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('donation-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    document.getElementById('success-message').style.display = 'block';
+});
