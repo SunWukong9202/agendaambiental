9930dbcea521ca0c9ba18d8b22fbb5a018aa088c
santiago <azahel_santiago@hotmail.com>
2025-08-25 18:31:57 -0600

    Update app to use htpps on production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -29,6 +30,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+
+        if(env('APP_ENV') != 'local') {
+            URL::forceSchme('https');
+        }
 
         DatabaseNotifications::pollingInterval('35s');
         // DatabaseNotifications::trigger('livewire.notifications-trigger');
